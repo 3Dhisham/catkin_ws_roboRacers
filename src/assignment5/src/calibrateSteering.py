668716cfec4f78097163d62759c7f66daf6b0a6b
Hisham Chbeib <hisham.chbeib@capgemini.com>
2019-11-19 10:31:25 +0100

    assignment 5 - final commit

--- src/assignment5/src/calibrateSteering.py
+++ src/assignment5/src/calibrateSteering.py
@@ -14,12 +14,15 @@ class CalibrateSteering:
     # Initiate node and subscribers
     def __init__(self):
         rospy.init_node("calibrate_steering")
+		
         # This subscribes to the ceiling camera topic which prints out the points based on car location
         self.localization_subscriber = rospy.Subscriber("/communication/gps/5", Odometry, self.read_points, queue_size=10)
-        # Steering angel topic
-        # self.angle_subscriber = rospy.Subscriber("/sensors/arduino/steering_angle", SteeringFeedback, self.steering_angle,queue_size=10)
+        
+		
+        self.angle_subscriber = rospy.Subscriber("/sensors/arduino/steering_angle", SteeringFeedback, self.steering_angle,queue_size=10)
         self.speed_publisher = rospy.Publisher("/actuators/speed", SpeedCommand, queue_size=10)
-        self.steering_publisher = rospy.Publisher("/actuators/steering_normalized", NormalizedSteeringCommand,
+        
+		self.steering_publisher = rospy.Publisher("/actuators/steering_normalized", NormalizedSteeringCommand,
                                               queue_size=10)
         # The spin() function runs the ros node in loops 
         rospy.spin()
@@ -53,20 +56,13 @@ class CalibrateSteering:
         print("z: ", z_point)
         print("=======================================================")
 
-
-# In here, we need to let the car drive in a circle, and while it is driving and changing position, 
-# we need to record three different positions with (x,y,z). 
-
-# With these three points, we can calculate the radius and the steering angle.
-# Steering angle can be calculated with 
-# 	angle w = math.atan(radius/0.27)   //comment: 27 cm distanz zw hinter Achse und vorder Achse
-
+ 
+		
 
 if __name__ == '__main__':
     try:
         steering = CalibrateSteering()
         steering.drive(0.8, 0.1)
-        #rospy.spin()
-        #CalibrateSteering()
+		
     except rospy.ROSInterruptException:
         pass
